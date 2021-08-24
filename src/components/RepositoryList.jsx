b5ed9e0f9f77926b2897f8a6fb0219b870b24c6e
Giovani <giovanimcorrea18@gmail.com>
2021-08-24 15:43:44 -0300

    learning useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,25 +1,31 @@
 import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/repositories.scss";
-
-const repository = {
-  name: "CV | Giovani Machado Corrêa",
-  description: "Meu cvzinho lindooo aiii",
-  link: "https://github.com/elesiann/webcurriculum",
-};
+import { useState, useEffect } from "react";
 
 export function RepositoryList() {
+  // useState é um Hook que te permite adicionar o state do React a um componente de função
+  const [repositories, setRepositories] = useState([]);
+
+  // useEffect dispara quando algo acontecer na aplicação (uma variável que muda, por exemplo)
+  // qual funçao executar () => {}, quando quero executar []
+  // CUIDADO PARA NAO DEIXAR SEM O SEGUNDO PARAMETRO, VAI ENTRAR EM LOOP AAAAAAAAAAAAAAAAAAAAAAAA
+  useEffect(() => {
+    fetch("https://api.github.com/users/Elesiann/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, [repositories]);
+
+  // .map() percorre cada um dos reps e pra cada um ele retorna alguma coisa
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
-
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
       </ul>
     </section>
   );
