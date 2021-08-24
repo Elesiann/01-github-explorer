feae52b5d34a8945e620eaeb8cda7fa498d11582
Giovani <giovanimcorrea18@gmail.com>
2021-08-23 21:39:13 -0300

    hooks, props, imutability and fast refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { Counter } from "./components/Counter";
 import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss";
 
 export function App() {
-  return <RepositoryList />;
+  return (
+    //sempre deve existir um conteiner pai acima de duas expressoes jsx
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  );
 }
